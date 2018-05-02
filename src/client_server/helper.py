5994e754614177680d43dd6e23dcf07a6fe9819c
zquangu112z <zquangu112z@gmail.com>
2018-05-02 22:34:18 +0700

    feat: arrange the lobby staff

--- src/client_server/helper.py
+++ src/client_server/helper.py
@@ -9,14 +9,64 @@ def load_dataframe(filename):
 
 
 def arrange_staff(dfStaff, dfRoom):
-    if(len(dfRoom) > len(dfStaff)):
+    # shuffle the list of staff
+    dfStaff = dfStaff.sample(frac=1)
+    if(len(dfRoom) > 2 * len(dfStaff)):
         print("There is not enough staff. Please add more staff.")
         return None
 
-    dfStaff.rename(columns={dfStaff.columns[3]: 'Phòng thi'}, inplace=True)
-    dfStaff[dfStaff.columns[3]] = pd.Series(dfRoom[:len(dfStaff)],
-                                            index=dfStaff.index)
-    return dfStaff
+    len_room = len(dfRoom)
+
+    # GIÁM THỊ 1
+    dfRoom.insert(
+        loc=len(dfRoom.iloc[0]),
+        column="Giám thị 1",
+        value=dfStaff[dfStaff.columns[1]][0:len_room].values)
+
+    dfRoom.insert(
+        loc=len(dfRoom.iloc[0]),
+        column="Mã số giám thị 1",
+        value=dfStaff[dfStaff.columns[0]][:len_room].values)
+
+    # GIÁM THỊ 2
+    dfRoom.insert(
+        loc=len(dfRoom.iloc[0]),
+        column="Giám thị 2",
+        value=dfStaff[dfStaff.columns[1]][len_room:2 * len_room].values)
+
+    dfRoom.insert(
+        loc=len(dfRoom.iloc[0]),
+        column="Mã số giám thị 2",
+        value=dfStaff[dfStaff.columns[0]][len_room:2 * len_room].values)
+
+    # GIÁM THỊ HÀNH LANG @TODO: optimize the code
+    nonAssignedStaff = dfStaff[dfStaff.columns[1]][2 * len(dfRoom):].values
+    nonAssignedStaffCode = dfStaff[dfStaff.columns[0]][2 * len(dfRoom):].values
+
+    num_lobby = int(len_room / len(nonAssignedStaff))
+    dfStaffLobby = []
+    dfStaffLobbyCode = []
+    for i in range(len(nonAssignedStaff) - 1):
+        for j in range(num_lobby):
+            dfStaffLobby.append(nonAssignedStaff[i])
+            dfStaffLobbyCode.append(nonAssignedStaffCode[i])
+
+    for i in range(num_lobby * (len(nonAssignedStaff) - 1), len_room):
+        dfStaffLobby.append(nonAssignedStaff[len(nonAssignedStaff) - 1])
+        dfStaffLobbyCode.append(
+            nonAssignedStaffCode[len(nonAssignedStaffCode) - 1])
+
+    dfRoom.insert(
+        loc=len(dfRoom.iloc[0]),
+        column="Giám thị hành lang",
+        value=dfStaffLobby)
+
+    dfRoom.insert(
+        loc=len(dfRoom.iloc[0]),
+        column="Mã số giá thị hành lang",
+        value=dfStaffLobbyCode)
+
+    return dfRoom
 
 
 def send_1_file_ftp(filename, socket):
--- src/client_server/server.py
+++ src/client_server/server.py
@@ -8,7 +8,7 @@ ROOM_FILENAME = "src/client_server/tmp/server_room.xlsx"
 RESULT_FILENAME = "src/client_server/tmp/result.xlsx"
 
 host = ''
-port = 12345
+port = 12354
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
 s.listen(5)
@@ -27,16 +27,16 @@ while True:
     dfStaff = dfStaff[dfStaff.columns[1:5]]
     # print(dfStaff.head())
     '''
-       Mã số  cán bộ                Họ Tên Trường  Ngày sinh
-    0      106170001          Mai Chiếm An   ĐHBK        NaN
-    1      106170002        Nguyễn Bảo Anh   ĐHBK        NaN
-    2      106170003    Phan Thị Quỳnh Anh   ĐHBK        NaN
-    3      106170004  Dương Văn Thanh Bình   ĐHBK        NaN
-    4      106170005       Lương Hữu Chung   ĐHBK        NaN
+       Mã số  cán bộ                Họ Tên Trường
+    0      106170001          Mai Chiếm An   ĐHBK
+    1      106170002        Nguyễn Bảo Anh   ĐHBK
+    2      106170003    Phan Thị Quỳnh Anh   ĐHBK
+    3      106170004  Dương Văn Thanh Bình   ĐHBK
+    4      106170005       Lương Hữu Chung   ĐHBK
     ...
     '''
     dfRoom = load_dataframe(ROOM_FILENAME)
-    dfRoom = dfRoom[dfRoom.columns[1]]
+    dfRoom = pd.DataFrame(dfRoom[dfRoom.columns[1]])
     # print(dfRoom.head())
     '''
     0    C201
@@ -46,15 +46,15 @@ while True:
     4    C205
     ...
     '''
-    dfStaff = arrange_staff(dfStaff, dfRoom)
+    dfRoom = arrange_staff(dfStaff, dfRoom)
     print(">>>>>>>>The result will be available at {}".format(RESULT_FILENAME))
     print("================")
-    print(dfStaff[dfStaff.columns[1:5]].head())
+    print(dfRoom.head())
     print("================")
 
     # Save result to ./tmp
     writer = pd.ExcelWriter(RESULT_FILENAME)
-    dfStaff.to_excel(writer, 'Sheet1')
+    dfRoom.to_excel(writer, 'Sheet1')
     writer.save()
 
     # Send result back to client
